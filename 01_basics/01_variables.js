b69e6cb4abf2e6fa9f1ee46b0ed9161cbb1e8a1c
Vihanga Wagholkar <vihanga.wagholkar@gmail.com>
2024-01-15 13:05:25 +0530

    First commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "vihanga.wagholkar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = 2; Not allowed 
+accountEmail = "vihanga.209302032@muj.manipal.edu"
+accountPassword = "7515"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
