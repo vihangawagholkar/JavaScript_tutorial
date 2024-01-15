afd98add5fbef170b95d42bc190ff56c7dd4c9cb
Vihanga Wagholkar <vihanga.wagholkar@gmail.com>
2024-01-15 17:22:51 +0530

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as  newer version
+
+// alert(3+3) // we are using nodejs not browser 
+
+console.log(3
+    +3) // code readability should be high
+console.log("Vihanga")
+
+let name = "Vihanga"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bigInt
+// boolean => true/false
+// string => ""
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
